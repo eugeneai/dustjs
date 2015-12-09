01f59265eba561acde3dc02da48edc90f3d9c7fa
Evgeny Cherkashin <eugeneai@irnok.net>
2015-12-09 11:14:29 +0800

    Removed redundant replacement.

--- src/build.js
+++ src/build.js
@@ -7,7 +7,7 @@ var parser_src = PEG.buildParser(
     fs.readFileSync(path.join(root, 'src', 'dust.pegjs'), 'utf8'), {output:"source"});
 
 fs.writeFileSync(path.join(root, 'lib', 'parser.js'), "(function(dust){\n\nvar parser = "
-  + parser_src.replace('this.SyntaxError', 'SyntaxError') + ";\n\n"
+  + parser_src + ";\n\n"
   + "dust.parse = parser.parse;\n\n"
   + "})(typeof exports !== 'undefined' ? exports : window.dust);"
 );
