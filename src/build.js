c3321ae25e958221ca4d356d9a28ba936d82c071
Evgeny Cherkashin <eugeneai@irnok.net>
2015-12-09 11:57:35 +0800

    Adjust parser generation to new pegjs.

--- src/build.js
+++ src/build.js
@@ -5,15 +5,16 @@ var peg  = require('pegjs'),
 
 var options = {
     cache: false,
-    trackLineAndColumn: true
+    trackLineAndColumn: true,
+    output:"source"
 };
 
-var parser = peg.buildParser(fs.readFileSync(path.join(root, 'src', 'dust.pegjs'), 'utf8'), options);
+var parser_src = peg.buildParser(fs.readFileSync(path.join(root, 'src', 'dust.pegjs'), 'utf8'), options);
 
 var namespace = 'parser';
 
 fs.writeFileSync(path.join(root, 'lib', 'parser.js'), "(function(dust){\n\nvar "+namespace+" = "
-  + parser.toSource().replace('this.SyntaxError', ''+namespace+'.SyntaxError') + ";\n\n"
+  + parser_src.replace('this.SyntaxError', ''+namespace+'.SyntaxError') + ";\n\n"
   + "dust.parse = "+namespace+".parse;\n\n"
   + "})(typeof exports !== 'undefined' ? exports : getGlobal());"
 );
