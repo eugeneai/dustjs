9c7353e42aff4c390ee29b613c87ab886722f2f9
Evgeny Cherkashin <eugeneai@irnok.net>
2015-12-09 10:11:07 +0800

    Parser generator adjusted to current pegjs.

--- src/build.js
+++ src/build.js
@@ -1,12 +1,13 @@
-var peg  = require('peg'),
+var PEG  = require('pegjs'),
     fs   = require('fs'),
     path = require('path'),
     root = path.join(path.dirname(__filename), "..");
 
-var parser = peg.buildParser(fs.readFileSync(path.join(root, 'src', 'dust.pegjs'), 'utf8'));
+var parser_src = PEG.buildParser(
+    fs.readFileSync(path.join(root, 'src', 'dust.pegjs'), 'utf8'), {output:"source"});
 
 fs.writeFileSync(path.join(root, 'lib', 'parser.js'), "(function(dust){\n\nvar parser = "
-  + parser.toSource().replace('this.SyntaxError', 'SyntaxError') + ";\n\n"
+  + parser_src.replace('this.SyntaxError', 'SyntaxError') + ";\n\n"
   + "dust.parse = parser.parse;\n\n"
   + "})(typeof exports !== 'undefined' ? exports : window.dust);"
 );
